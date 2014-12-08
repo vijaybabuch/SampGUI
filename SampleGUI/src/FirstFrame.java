f4e9512a8d738236cf88c763e1c60cd5b193924f
vbchintapalli <vbchintapalli@A7015E1D.corp.ads.valuelabs.net>
2014-12-08 17:08:21 +0530

    My First Frame Commit

---
+++ SampleGUI/src/FirstFrame.java
@@ -0,0 +1,124 @@
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.border.EtchedBorder;
+
+import org.dyno.visual.swing.layouts.Constraints;
+import org.dyno.visual.swing.layouts.GroupLayout;
+import org.dyno.visual.swing.layouts.Leading;
+
+
+//VS4E -- DO NOT REMOVE THIS LINE!
+public class FirstFrame extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JLabel jLabel0;
+	private JPanel jPanel0;
+	private JPanel jPanel1;
+	private JLabel jLabel1;
+	private JTextField jTextField0;
+	private JButton jButton0;
+	private static final String PREFERRED_LOOK_AND_FEEL = "javax.swing.plaf.metal.MetalLookAndFeel";
+	public FirstFrame() {
+		initComponents();
+	}
+
+	private void initComponents() {
+		setLayout(new GroupLayout());
+		add(getJPanel0(), new Constraints(new Leading(3, 441, 10, 10), new Leading(6, 34, 10, 10)));
+		add(getJPanel1(), new Constraints(new Leading(5, 436, 12, 12), new Leading(46, 168, 10, 10)));
+		setSize(449, 221);
+	}
+
+	private JButton getJButton0() {
+		if (jButton0 == null) {
+			jButton0 = new JButton();
+			jButton0.setText("Submit");
+		}
+		return jButton0;
+	}
+
+	private JTextField getJTextField0() {
+		if (jTextField0 == null) {
+			jTextField0 = new JTextField();
+		}
+		return jTextField0;
+	}
+
+	private JLabel getJLabel1() {
+		if (jLabel1 == null) {
+			jLabel1 = new JLabel();
+			jLabel1.setText("Enter Product ID :");
+		}
+		return jLabel1;
+	}
+
+	private JPanel getJPanel1() {
+		if (jPanel1 == null) {
+			jPanel1 = new JPanel();
+			jPanel1.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED, null, null));
+			jPanel1.setLayout(new GroupLayout());
+			jPanel1.add(getJLabel1(), new Constraints(new Leading(27, 113, 10, 10), new Leading(27, 22, 12, 12)));
+			jPanel1.add(getJTextField0(), new Constraints(new Leading(152, 128, 12, 12), new Leading(25, 27, 12, 12)));
+			jPanel1.add(getJButton0(), new Constraints(new Leading(301, 10, 10), new Leading(27, 12, 12)));
+		}
+		return jPanel1;
+	}
+
+	private JPanel getJPanel0() {
+		if (jPanel0 == null) {
+			jPanel0 = new JPanel();
+			jPanel0.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED, null, null));
+			jPanel0.setLayout(new GroupLayout());
+			jPanel0.add(getJLabel0(), new Constraints(new Leading(175, 85, 10, 10), new Leading(5, 10, 10)));
+		}
+		return jPanel0;
+	}
+
+	private JLabel getJLabel0() {
+		if (jLabel0 == null) {
+			jLabel0 = new JLabel();
+			jLabel0.setText("App Title Lable");
+		}
+		return jLabel0;
+	}
+
+	private static void installLnF() {
+		try {
+			String lnfClassname = PREFERRED_LOOK_AND_FEEL;
+			if (lnfClassname == null)
+				lnfClassname = UIManager.getCrossPlatformLookAndFeelClassName();
+			UIManager.setLookAndFeel(lnfClassname);
+		} catch (Exception e) {
+			System.err.println("Cannot install " + PREFERRED_LOOK_AND_FEEL
+					+ " on this platform:" + e.getMessage());
+		}
+	}
+
+	/**
+	 * Main entry of the class.
+	 * Note: This class is only created so that you can easily preview the result at runtime.
+	 * It is not expected to be managed by the designer.
+	 * You can modify it as you like.
+	 */
+	public static void main(String[] args) {
+		installLnF();
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				FirstFrame frame = new FirstFrame();
+				frame.setDefaultCloseOperation(FirstFrame.EXIT_ON_CLOSE);
+				frame.setTitle("FirstFrame");
+				frame.getContentPane().setPreferredSize(frame.getSize());
+				frame.pack();
+				frame.setLocationRelativeTo(null);
+				frame.setVisible(true);
+			}
+		});
+	}
+
+}
